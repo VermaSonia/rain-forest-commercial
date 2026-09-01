be2215e42b06e87b9afc72d11d59d0ed36e011d2
VermaSonia <46300457+VermaSonia@users.noreply.github.com>
2026-09-01 15:20:12 -0600

    templte files created.

--- page-templates/page-case-studies.php
+++ page-templates/page-case-studies.php
@@ -1,39 +1,20 @@
 <?php
   global $post;
 /*
-  Template Name: Home
+  Template Name: Case Studies
 */
 /**
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package Rain Forest Commercial
  */
-/****************************
-
- * QUERY FEATURED RECIPES
-
- ***************************/
-
-$recipeArgs = [
-  'post_type' => 'recipe',
-  'post_status' => 'publish',
-  'posts_per_page' => -1,
-  'meta_query' => [
-    [
-      'key' => 'featured',
-      'value' => true,
-      'Compare' => 'IN'
-    ]
-  ]
-];
 
 /****************************************************
  * STORE ALL LOGIC IN CONTEXT AND OUTPUT TO VIEW
  ***************************************************/
 $context = Timber::context();
 $context['field'] = Timber::get_post();
-$context['recipes'] = Timber::get_posts($recipeArgs);
-$templates = array( 'page-home.twig' );
+$templates = array( 'page-case-study.twig' );
 Timber::render( $templates, $context );
 
 
