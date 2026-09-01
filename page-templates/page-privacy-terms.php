eb72fa01dd641d1fe77d8a302ae9cad317d8e4d2
VermaSonia <46300457+VermaSonia@users.noreply.github.com>
2026-09-01 16:12:01 -0600

    privach pgae added

---
+++ page-templates/page-privacy-terms.php
@@ -0,0 +1,18 @@
+<?php
+  global $post;
+/*
+  Template Name: Privacy Policy & Terms and Conditions
+*/
+/**
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Rain Forest Commercial
+ */
+
+/****************************************************
+ * STORE ALL LOGIC IN CONTEXT AND OUTPUT TO VIEW
+ ***************************************************/
+$context = Timber::context();
+$context['field'] = Timber::get_post();
+$templates = array( 'page-privacy-terms.twig' );
+Timber::render( $templates, $context );
