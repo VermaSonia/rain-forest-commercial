0d483089058a0bf487f6157277059d750e5db3bd
VermaSonia <46300457+VermaSonia@users.noreply.github.com>
2026-09-21 13:48:07 -0600

    case studies page done

--- page-templates/page-case-studies.php
+++ page-templates/page-case-studies.php
@@ -11,7 +11,7 @@
 
 /****************************************************
  * STORE ALL LOGIC IN CONTEXT AND OUTPUT TO VIEW
- ***************************************************/
+***************************************************/
 $context = Timber::context();
 $context['field'] = Timber::get_post();
 $templates = array( 'page-case-study.twig' );
